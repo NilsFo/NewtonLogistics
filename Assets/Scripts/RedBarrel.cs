f5ebe2da2f6b2d7075e12c08940d96c709a7c1cf
Nils Fo <nils.foerster@yahoo.de>
2023-04-29 13:28:27 +0200

    Updating scene, gameplay objects, etc

---
+++ Assets/Scripts/RedBarrel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBarrel : MonoBehaviour
+{
+    public bool explodeable;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
